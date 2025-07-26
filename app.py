d021ec543f826e3f98152b019ee649ca8a8ab37f
christiansrgit <whyytorrrr@gmail.com>
2025-07-25 21:37:04 -0300

    Fix: Bind a 0.0.0.0 y puerto dinámico para Render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from datetime import datetime
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -30,4 +31,7 @@ def log_data():
     return {'status': 'ok'}, 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
